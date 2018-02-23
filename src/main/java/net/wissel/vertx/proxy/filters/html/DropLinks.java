ad590d64af4bc4b89d89321311c7983463a4eb67
Stephan H. Wissel <stw@linux.com>
2018-02-23 19:19:14 +0800

    add subfilter for html

---
+++ src/main/java/net/wissel/vertx/proxy/filters/html/DropLinks.java
@@ -0,0 +1,45 @@
+/** ========================================================================= *
+ * Copyright (C)  2017, 2018 Salesforce Inc ( http://www.salesforce.com/      *
+ *                            All rights reserved.                            *
+ *                                                                            *
+ *  @author     Stephan H. Wissel (stw) <swissel@salesforce.com>              *
+ *                                       @notessensei                         *
+ * @version     1.0                                                           *
+ * ========================================================================== *
+ *                                                                            *
+ * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
+ * not use this file except in compliance with the License.  You may obtain a *
+ * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
+ *                                                                            *
+ * Unless  required  by applicable  law or  agreed  to  in writing,  software *
+ * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
+ * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
+ * License for the  specific language  governing permissions  and limitations *
+ * under the License.                                                         *
+ *                                                                            *
+ * ========================================================================== *
+ */
+package net.wissel.vertx.proxy.filters.html;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import net.wissel.vertx.proxy.filters.HtmlSubFilter;
+
+/**
+ * Removes all link targets URLs from a page
+ * @author swissel
+ *
+ */
+public class DropLinks implements HtmlSubFilter {
+
+    @Override
+    public void apply(Document doc) {
+        Elements links = doc.getElementsByTag("a");
+        links.forEach(e -> {
+            e.attr("href", "#");
+        });
+
+    }
+
+}
