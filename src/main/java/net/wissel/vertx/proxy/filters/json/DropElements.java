4a4f8b99634fb5ed747eca9e7d29e7e69d209794
Stephan H. Wissel <stw@linux.com>
2018-02-25 19:11:37 +0800

    Introduce JSON filters

---
+++ src/main/java/net/wissel/vertx/proxy/filters/json/DropElements.java
@@ -0,0 +1,41 @@
+/** ========================================================================= *
+ * Copyright (C)  2017, 2018 Salesforce Inc ( http://www.salesforce.com/      *
+ *                            All rights reserved.                            *
+ *                                                                            *
+ *  @author     Stephan H. Wissel (stw) <swissel@salesforce.com>              *
+ *                                       @notessensei                         *
+ * @version     1.0                                                           *
+ * ========================================================================== *
+ *                                                                            *
+ * Licensed under the  Apache License, Version 2.0  (the "License").  You may *
+ * not use this file except in compliance with the License.  You may obtain a *
+ * copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>.       *
+ *                                                                            *
+ * Unless  required  by applicable  law or  agreed  to  in writing,  software *
+ * distributed under the License is distributed on an  "AS IS" BASIS, WITHOUT *
+ * WARRANTIES OR  CONDITIONS OF ANY KIND, either express or implied.  See the *
+ * License for the  specific language  governing permissions  and limitations *
+ * under the License.                                                         *
+ *                                                                            *
+ * ========================================================================== *
+ */
+package net.wissel.vertx.proxy.filters.json;
+
+import io.vertx.core.json.JsonObject;
+import net.wissel.vertx.proxy.filters.JsonSubFilter;
+
+/**
+ * get rid of Json elements based on a JSON path
+ * 
+ * @author swissel
+ *
+ */
+public class DropElements implements JsonSubFilter {
+
+    @Override
+    public void apply(final JsonObject source) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
